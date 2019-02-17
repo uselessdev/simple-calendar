631ad883b992a624e0a4be734852b31cc3ddff5d
Wallace Oliveira <wallace.oliveira@pagar.me>
2019-02-17 18:16:42 -0300

    Updates

---
+++ precache-manifest.2dcf113606975de306950fbcde478b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19c073cf95344771951b",
+    "url": "/simple-calendar/static/js/runtime~main.19c073cf.js"
+  },
+  {
+    "revision": "fd69b7447bcb78ead716",
+    "url": "/simple-calendar/static/js/main.fd69b744.chunk.js"
+  },
+  {
+    "revision": "9347b26a4590ed088fa7",
+    "url": "/simple-calendar/static/js/2.9347b26a.chunk.js"
+  },
+  {
+    "revision": "fd69b7447bcb78ead716",
+    "url": "/simple-calendar/static/css/main.81cf212a.chunk.css"
+  },
+  {
+    "revision": "14115d2b8b58b33759b82f4a6909301e",
+    "url": "/simple-calendar/index.html"
+  }
+];
